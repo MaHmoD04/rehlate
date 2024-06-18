374fc031740f2b4626d821325f706b86209e828b
Mahmoud S Albohaisi <mohabohaisi@gmail.com>
2024-06-19 00:36:15 +0300

    fix problem slider 1

--- js/custom.js
+++ js/custom.js
@@ -14,10 +14,6 @@ barsBtn.addEventListener("click", function () {
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
-
-  autoplay: {
-    delay: 2000,
-  },
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
